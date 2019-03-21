a5a90b355f70d3d067ab1cbf43eda7a598ec5343
A1ex <jaroshov@ukr.net>
2019-03-21 20:53:27 +0200

    21032019

---
+++ app/src/main/java/android/a1ex/com/criminalintent/Crime.java
@@ -0,0 +1,66 @@
+package android.a1ex.com.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private Boolean mSolved;
+    private String mSuspect;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+        mTitle = "";
+        mSolved = false;
+    }
+
+    public Crime(UUID id) {
+        mId = id;
+        mDate = new Date();
+        mTitle = "";
+        mSolved = false;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
+    }
+
+    public String getPhotoFilename() {
+        return "IMG_" + getId().toString() + ".jpg";
+    }
+
+    public Boolean getSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(Boolean solved) {
+        mSolved = solved;
+    }
+}
---
+++ app/src/main/java/android/a1ex/com/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package android.a1ex.com.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
